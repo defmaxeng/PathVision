444a336aaba914fbb76f05f08ef28bf04cb1d613
Max Eng <maxeng017@gmail.com>
2025-04-13 22:47:55 -0400

    fixed some stuff

---
+++ test3.py
@@ -0,0 +1,10 @@
+from nuscenes.nuscenes import NuScenes
+
+# Set the dataset directory
+dataset_directory = "/path/to/your/nuscenes/dataset"
+
+# Initialize NuScenes object
+nusc = NuScenes(version='v1.0-mini', dataroot=dataset_directory, verbose=True)
+
+# Print out information on the dataset
+print("Available sample data:", len(nusc.sample))
